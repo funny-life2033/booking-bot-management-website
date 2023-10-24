8f972759cd5fba75621ad0b5e7b1c31b10ad799d
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-10-25 06:31:02 +0900

    convert ws to https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 // export const WEB_SOCKET_HOST = "ws://192.168.148.59:5000/";
 export const WEB_SOCKET_HOST =
-  "ws://booking-bot-management-websocket-server.onrender.com/";
+  "https://booking-bot-management-websocket-server.onrender.com/";
