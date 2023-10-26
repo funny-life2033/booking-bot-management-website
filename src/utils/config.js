86cc76e8d9b4b30db37858e3a4dcda1edd89cc99
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-10-27 01:18:12 +0900

    ws to wss & http to https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// export const BASE_URL = "booking-bot-management-websocket-server.onrender.com";
-export const BASE_URL = "192.168.148.59:5000";
-export const WEB_SOCKET_HOST = `ws://${BASE_URL}/`;
-export const Axios = axios.create({ baseURL: `http://${BASE_URL}` });
+export const BASE_URL = "booking-bot-management-websocket-server.onrender.com";
+// export const BASE_URL = "192.168.148.59:5000";
+export const WEB_SOCKET_HOST = `wss://${BASE_URL}/`;
+export const Axios = axios.create({ baseURL: `https://${BASE_URL}` });
 // export const WEB_SOCKET_HOST =
 //   "wss://booking-bot-management-websocket-server.onrender.com/";
 
